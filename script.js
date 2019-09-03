d75b87b58ae57c53c868ed57441f905938d7a4fb
subrat-google <subratverma3356@gmail.com>
2019-09-03 14:59:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var myapp = new Vue({   //event Handling in vue js
+    el: "#my-app",
+    data:{
+  counter: 0
+   
+    },
+    methods:{
+        increment: function(inc){
+this.counter = this.counter + inc;
+        },
+        decrement: function(desc){
+            this.counter = this.counter - desc;
+        }
+    }
+});
